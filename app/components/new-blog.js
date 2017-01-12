c2025c5c799f815e9410c95e0d48554a4dbccb9f
Steve-Burton <stevevburton@gmail.com>
2017-01-11 19:58:56 -0800

    add functionality to create a new blog

---
+++ app/components/new-blog.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    saveBlog() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "",
+        title: this.get('title') ? this.get('title') : "",
+        body: this.get('body') ? this.get('body') : "",
+        image: this.get('image') ? this.get('image') : "",
+        credit: this.get('credit') ? this.get('credit') : "",
+      };
+      this.sendAction('saveBlog', params);
+    }
+  }
+});
--- app/routes/blog.js
+++ app/routes/blog.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // actions: {
-  //   saveBlog(params) {
-  //     var newBlog = this.store.createRecord('blog', params);
-  //     newBlog.save();
-  //     this.transitionTo('index');
-  //   }
-  // }
+  actions: {
+    saveBlog(params) {
+      var newBlog = this.store.createRecord('blog', params);
+      newBlog.save();
+      this.transitionTo('index');
+    }
+  },
 });
